b560b13207836254c1abcf716eca3ac31c0e20e3
VladimirWillians <47118243+JDevWill@users.noreply.github.com>
2023-12-15 16:38:12 -0500

    Add files via upload

---
+++ api.js
@@ -0,0 +1,11 @@
+const getImagen = async() =>{
+    const apiKey ='ba70zQMX6yJYDryvhUe3jzWNPNGYrY2Y';
+    const resp = await fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+    const {data} = await resp.json(); // es necesario siempre destrcuturar la data
+    const {url} = data.images.original  // data.data.images.original codigo aternario si no se destructura la data
+    const img = document.createElement('img')
+    img.src = url
+    document.body.append(img)
+}
+
+getImagen()
